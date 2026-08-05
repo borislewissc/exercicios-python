8a17612872a3665f09ebe4a138abd775f012ae3c
Boris Lewis <borislewissc@gmail.com>
2026-08-05 19:17:52 -0300

    feat(curso-em-video): resolve exercicio 26 do mundo 1

---
+++ curso-em-video/mundo-1/ex026_posição_letra.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra “A”, em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+
+frase = str(input('Digite uma frase: ')).upper().strip()
+print(f'Sua frase possui {frase.count('A')} letras A')
+print(f'A primeira letra A está na posição {frase.find("A") + 1}')
+print(f'A última letra A está na posição {frase.rfind('A') + 1}')
