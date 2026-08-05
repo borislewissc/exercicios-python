083e8104ae5d098fdb87e0f6fd85a3ab9c632c17
Boris Lewis <borislewissc@gmail.com>
2026-08-05 18:59:11 -0300

    feat(curso-em-video): resolve exercicio 25 do mundo 1

---
+++ curso-em-video/mundo-1/ex025_procurando_string_silva.py
@@ -0,0 +1,4 @@
+# Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
+
+name = str(input('Digite seu nome completo: ')).strip()
+print(f'Seu nome tem Silva? {'SILVA' in name.upper()}')
