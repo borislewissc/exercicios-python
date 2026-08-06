a667875953eb3ba3964dfae381743e45e4df28af
Boris Lewis <borislewissc@gmail.com>
2026-08-06 10:56:54 -0300

    feat(curso-em-video): resolve exercicio 29 do mundo 1

---
+++ curso-em-video/mundo-1/ex029_radar_eletronico.py
@@ -0,0 +1,8 @@
+# Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
+
+velocidade = int(input('Qual a velocidade do carro?'))
+if velocidade > 80:
+    print('Você foi multado por exceder o limite de 80km/h!')
+    multa = (velocidade - 80) * 7
+    print(f'Valor da multa: R${multa}')
+print('Tenha um bom dia! Dirija com segurança.')
