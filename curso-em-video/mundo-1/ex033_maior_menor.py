30f6d26608ceb1724096a08a0fe81f9955923388
Boris Lewis <borislewissc@gmail.com>
2026-08-06 11:43:12 -0300

    feat(curso-em-video): resolve exercicio 34 do mundo 1

---
+++ curso-em-video/mundo-1/ex033_maior_menor.py
@@ -0,0 +1,19 @@
+# Faça um programa que leia três numeros e mostre qual é o maior e qual é o menor.
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+num3 = int(input('Digite o terceiro número: '))
+
+menor = num1
+if num2 < num1 and num2 < num3:
+    menor = num2
+if num3 < num1  and num3 < num2:
+    menor = num3
+
+maior = num2
+if num1 > num2 and num1 > num3:
+    maior = num1
+if num3 > num2 and num3 > num1:
+    maior = num3
+
+print(f'O maior número é {maior} e o menor número é {menor}.')
